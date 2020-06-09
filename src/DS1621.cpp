b1ef11a4b99d8cca85e99aaaa62b821953e0c068
Derrick Marlow <derrick.marlow@yahoo.com>
2020-06-08 17:24:22 -0700

    Update DS1621.cpp

--- src/DS1621.cpp
+++ src/DS1621.cpp
@@ -8,7 +8,7 @@ void DS1621::begin() {
   Wire.begin();
   Wire.beginTransmission(_address);
   Wire.write(0xAC);
-  Wire.write(0);
+  Wire.write(0x02);
   Wire.beginTransmission(_address);
   Wire.write(0xEE);
   Wire.endTransmission();
@@ -19,11 +19,7 @@ int16_t DS1621::getTempC() {
   Wire.write(0xAA);
   Wire.endTransmission(false);
   Wire.requestFrom(_address, 2);
-  uint8_t msb = Wire.read();
-  uint8_t lsb = Wire.read();
- 
-  int16_t t = (int8_t)msb << 1 | lsb >> 7;
-  t = t * 10/2;
+  uint8_t t = Wire.read();
   
   return t;  
 }
@@ -33,11 +29,7 @@ int16_t DS1621::getTempF() {
   Wire.write(0xAA);
   Wire.endTransmission(false);
   Wire.requestFrom(_address, 2);
-  uint8_t msb = Wire.read();
-  uint8_t lsb = Wire.read();
- 
-  int16_t t = (int8_t)msb << 1 | lsb >> 7;
-  t = t * 10/2;
+  uint8_t t = Wire.read();
   int16_t tF = t * 9/5 + 320;
   
   return tF;  
